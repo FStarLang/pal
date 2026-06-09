212049189782dd3930dcdfd9acb1f7458641b38a
Gabriel Ebner <gebner@gebner.org>
2026-06-09 20:53:31 +0000

    Improve test.
    Fixes #72.

--- test/array_update/array_update.c
+++ test/array_update/array_update.c
@@ -1,15 +1,43 @@
 #include "pal.h"
 #include <stdint.h>
+#include <stdlib.h>
 
 struct point {
     int x;
     int y;
 };
 
-void set_x(struct point arr[])
+void set_x_alt(struct point arr[])
     _requires(arr._length == 2)
 {
     arr[0].x = 42;
+}
+
+
+void set_x(_array struct point *pts, size_t i, int val)
+  _requires(i < pts._length)
+  _preserves_value(pts._length)
+  _ensures(pts[i].x == val)
+{
+  pts[i].x = val;
+}
+
+void set_y(_array struct point *pts, size_t i, int val)
+  _requires(i < pts._length)
+  _preserves_value(pts._length)
+  _ensures(pts[i].y == val)
+{
+  pts[i].y = val;
+}
+
+void set_both(_array struct point *pts, size_t i, int vx, int vy)
+  _requires(i < pts._length)
+  _preserves_value(pts._length)
+  _ensures(pts[i].x == vx)
+  _ensures(pts[i].y == vy)
+{
+  pts[i].x = vx;
+  pts[i].y = vy;
 }
 
 struct entry {
