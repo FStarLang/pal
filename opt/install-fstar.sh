0ea464924b42d0ec53c440e60f11f71886c49842
Gabriel Ebner <gebner@gebner.org>
2026-09-05 01:58:40 +0000

    Bump F*

--- opt/install-fstar.sh
+++ opt/install-fstar.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -fsSL https://aka.ms/install-fstar | bash -s -- --nightly --version 2026-08-27 "$@"
+curl -fsSL https://aka.ms/install-fstar | bash -s -- --nightly --version 2026-09-04 "$@"
